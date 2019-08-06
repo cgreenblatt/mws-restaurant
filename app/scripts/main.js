301188af3ce9322ea575e8afba65fb093b885784
cgreenblatt <cynthia_greenblatt@yahoo.com>
2019-08-06 16:02:26 -0700

    feat: Add restaurant map markers to map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,20 +2,54 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
   model = {
-    init: function() {
+    getURL() {
+      const port = 9000 // Change this to your server port
+      return `http://localhost:${port}/data/restaurants.json`;
+    },
+    getMapCenterCoords: function() {
       return {map: {center: [40.722216, -73.987501]}}
+    },
+    getRestaurantURL: function(id) {
+      return (`./restaurant.html?id=${id}`);
+    },
+    initData: function() {
+      return fetch(model.getURL())
+        .then(response => {
+          if (!response.ok)
+            throw(new Error(`Error: ${response.statusText}`));
+          return response.json();
+        })
+        .then((json) => {
+          this.data = json;
+          this.data.restaurants.forEach((restaurant) => {
+            restaurant.url = model.getRestaurantURL(restaurant.id);
+          });
+          console.log(this.data);
+          return this.data.restaurants;
+        })
+        .catch(error => {
+          console.log('opps ' + error);
+        });
     }
   }
 
   controller = {
     init: function() {
-      view.init(model.init());
-    },
-  }
+      model.initData().then((data) => {
+        view.init({
+          coordinates: model.getMapCenterCoords(),
+          restaurants: data,
+        });
+      });
+    }
+  };
 
   view = {
-    init: function(init) {
-      debugger;
+    init: function(initData) {
+      this.initMap(initData.coordinates);
+      this.addMarkers(initData.restaurants);
+    },
+    initMap: function(init) {
       this.newMap = L.map('map', {
         center: init.map.center,
         zoom: 12,
@@ -29,6 +63,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         id: 'mapbox.streets'
       }).addTo(this.newMap);
+    },
+    addMarkers(restaurants) {
+      restaurants.forEach((restaurant) => {
+          const marker = view.addMarker(restaurant);
+          view.addMarkerClickHandler(marker);
+      });
+    },
+    addMarker: function(restaurant) {
+      // https://leafletjs.com/reference-1.3.0.html#marker
+      const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
+        {
+          title: restaurant.name,
+          alt: restaurant.name,
+          url: restaurant.url,
+          id: restaurant.id,
+        })
+      marker.addTo(view.newMap);
+      return marker;
+    },
+    addMarkerClickHandler: (marker) => {
+      marker.on('click', view.markerClickHandler);
+    },
+    markerClickHandler: (event) => {
+      console.log('id is ' + event.sourceTarget.options.id);
     }
   }
 
