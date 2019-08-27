abb0b45733a565acc2984c4df7b440181c3a9d24
cgreenblatt <cynthia_greenblatt@yahoo.com>
2019-08-26 18:39:11 -0700

    fix: Address syntax issues and refactor some functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,23 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  model = {
+  const model = {
     getURL() {
-      const port = 9000 // Change this to your server port
+      const port = 9000; // Change this to your server port
       return `http://localhost:${port}/data/restaurants.json`;
     },
     getMapCenterCoords: () => (
       {
         map: {
-          center: [40.722216, -73.987501]
-        }
+          center: [40.722216, -73.987501],
+        },
       }),
     getRestaurantURL: (id) => `./restaurant.html?id=${id}`,
-    getImageURLs: (id) => ['-350-small', '-700-medium', '-1050-large', '-1400-xlarge'].map(size => `/images/${id}${size}.jpg`),
-    initData: function() {
+    getImageURLs: (id) => ['-350-small', '-700-medium', '-1050-large', '-1400-xlarge'].map((size) => `/images/${id}${size}.jpg`),
+    initData: function initData() {
       return fetch(this.getURL())
-        .then(response => {
-          if (!response.ok)
-            throw(new Error(`Error: ${response.statusText}`));
+        .then((response) => {
+          if (!response.ok) throw new Error(`Error: ${response.statusText}`);
           return response.json();
         })
         .then((json) => {
@@ -30,27 +29,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
           });
           return this.data.restaurants;
         })
-        .catch(error => {
+        .catch((error) => {
           console.log('oops ' + error);
         });
     },
-    getValuesFor: function(filterKey) {
+    getValuesFor: function getValuesFor(filterKey) {
       const reducer = (acc, restaurant) => {
         const value = restaurant[filterKey];
         if (acc[value]) {
           acc[value].push(restaurant.id - 1);
         } else {
-          acc[value] = [restaurant.id -1];
+          acc[value] = [restaurant.id - 1];
         }
         return acc;
-      }
+      };
       let acc = { All: [] };
-      for (let i = 0; i < this.data.restaurants.length; i++) {
+      for (let i = 0; i < this.data.restaurants.length; i += 1) {
         acc.All.push(i);
       }
-      const values = this.data.restaurants.reduce(reducer, acc);
-      this.data.filters[filterKey] = values;
-      return values;
+      acc = this.data.restaurants.reduce(reducer, acc);
+      this.data.filters[filterKey] = acc;
+      return acc;
     },
     getFilterIndexes(filterValuesSelected, filterKey) {
       const selectedValue = filterValuesSelected[filterKey];
@@ -58,55 +57,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
       const filterIndexes = filterValuesObject[selectedValue];
       return filterIndexes;
     },
-    getIntersection: function(array1, array2) {
-        return array2.filter(value => array1.includes(value));
+    getIntersection: function getIntersection(array1, array2) {
+      return array2.filter((value) => array1.includes(value));
     },
-    filterRestaurants: function(filterValuesSelected) {
+    filterRestaurants: function filterRestaurants(filterValuesSelected) {
       const filterKeys = Object.keys(filterValuesSelected);
       let intersectionIndexes = this.getFilterIndexes(filterValuesSelected, filterKeys[0]);
-      for (let i = 1; i < filterKeys.length; i++) {
+      for (let i = 1; i < filterKeys.length; i += 1) {
         const nextFilterIndexes = this.getFilterIndexes(filterValuesSelected, filterKeys[i]);
         intersectionIndexes = this.getIntersection(intersectionIndexes, nextFilterIndexes);
       }
       return intersectionIndexes;
-    }
-  }
-  controller = {
-    init: function() {
-      model.initData().then((data) => {
-        //this.restaurants = data;
-        view.init({
-          coordinates: model.getMapCenterCoords(),
-          restaurants: data,
-          filters: {
-            cuisines: this.getFilterOptions('cuisine_type'),
-            neighborhoods: this.getFilterOptions('neighborhood'),
-          }
-        });
-      });
-    },
-    getFilterOptions: function(filterKey) {
-      const values = model.getValuesFor(filterKey);
-      return {
-        filterKey,
-        values,
-      }
-    },
-    filterRestaurants: function(filterValues) {
-      const list = model.filterRestaurants(filterValues);
-      view.updateRestaurantList(list);
     },
   };
 
-  view = {
-    init: function(initData) {
-      this.initMap(initData.coordinates);
-      this.markerArray = this.createMarkers(initData.restaurants);
-      this.addMarkersToMap(this.markerArray);
+  const view = {
+    init: function init(controller, initData) {
+      this.controller = controller;
+      this.newMap = this.initMap(initData.coordinates);
+      this.markerArray = this.initMarkers(initData.restaurants);
       this.listContainer = document.getElementById('restaurants-container');
-      //this.restaurantHTMLArray = this.initRestaurantHTMLArray(initData.restaurants);
       this.restaurantElementsArray = this.initRestaurantElementsArray(initData.restaurants);
-      //this.addRestaurantsToDOM(this.restaurantHTMLArray);
       this.currentRestaurantUL = this.getRestaurantListElement(this.restaurantElementsArray);
       this.handleFilterSelection = this.handleFilterSelection.bind(this);
       this.addRestaurantListToDom(this.currentRestaurantUL);
@@ -114,57 +85,50 @@ window.addEventListener('DOMContentLoaded', (event) => {
       this.addFilter(initData.filters.cuisines, 'Cuisines');
       this.addFilter(initData.filters.neighborhoods, 'Neighborhoods');
     },
-    initMap: function(init) {
-      this.newMap = L.map('map', {
-        center: init.map.center,
+    initMap: (coordinates) => {
+      const newMap = L.map('map', {
+        center: coordinates.map.center,
         zoom: 12,
-        scrollWheelZoom: false
+        scrollWheelZoom: false,
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
         mapboxToken: 'pk.eyJ1Ijoic3BhcmtwdWciLCJhIjoiY2pqaGphZmZrMHpjZzNxcXN6NnFnODV1MCJ9.mguQAJA30rXv2JkSHo6Ntg',
         maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'
-      }).addTo(this.newMap);
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, '
+          + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '
+          + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets',
+      }).addTo(newMap);
+      return newMap;
     },
-    createMarkers: function(restaurants) {
-      return restaurants.map((restaurant) =>
-        new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
+    initMarkers: function initMarkers(restaurants) {
+      function markerClickHandler(event) {
+        console.log('id is ' + event.sourceTarget.options.id);
+      }
+      return restaurants.map((restaurant) => {
+        const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng],
           {
             title: restaurant.name,
             alt: restaurant.name,
             url: restaurant.restaurantURL,
             id: restaurant.id,
-          }).on('click', this.markerClickHandler)
-        )
+          });
+        marker.on('click', markerClickHandler);
+        marker.addTo(this.newMap);
+        return marker;
+      });
     },
-    addMarkersToMap: function(markers) {
-      markers.forEach(marker => marker.addTo(this.newMap))
-    },
-    markerClickHandler: (event) => {
-      console.log('id is ' + event.sourceTarget.options.id);
-    },
-    getImageElement: function(restaurant) {
+    getImageElement: function getImageElement(restaurant) {
       const img = document.createElement('img');
       img.alt = restaurant.name;
       img.src = restaurant.imageURLs[1];
-      img.srcset = this.getImageSrcset(restaurant);
-      img.sizes = this.getImageSizes();
+      img.srcset = restaurant.imageURLs.map((url) => `${url} ${url.split('-')[1]}w`).join(', ');
+      img.sizes = '(min-width: 700px) 45.5vw, (min-width: 1050px) 29.333vw, '
+        + '(min-width: 1400px) 21.25vw, (min-width: 1750px) 16.4vw';
       img.className = 'restaurant-img';
       return img;
     },
-    getImageHTML: function (restaurant) {
-      return `<img src="${restaurant.imageURLs[1]}" srcset="${this.getImageSrcset(restaurant)}" sizes="${this.getImageSizes()}" alt="${restaurant.name}" class="restaurant-img">\n`
-    },
-    getImageSrcset: restaurant => restaurant.imageURLs.map(url => `${url} ${url.split('-')[1]}w`).join(', '),
-    getImageSizes: () =>
-        '(min-width: 700px) 45.5vw, ' +
-        '(min-width: 1050px) 29.333vw, ' +
-        '(min-width: 1400px) 21.25vw, ' +
-        '(min-width: 1750px) 16.4vw',
-    getRestaurantLiElement: function(restaurant) {
+    getRestaurantLiElement: function getRestaurantLIElement(restaurant) {
       const name = document.createElement('h3');
       name.textContent = restaurant.name;
       const neighborhood = document.createElement('h4');
@@ -183,68 +147,73 @@ window.addEventListener('DOMContentLoaded', (event) => {
       li.appendChild(link);
       return li;
     },
-    /*getRestaurantHTML: function(restaurant) {
-      return `
-      <li class="restaurant-card" />
-        ${this.getImageHTML(restaurant)}
-        <h3>${restaurant.name}</h3>
-        <h4>${restaurant.neighborhood}</h4>
-        <h4>${restaurant.address}</h4>
-        <a href="${restaurant.restaurantURL}" aria-label="${restaurant.name} details">View Details</a>
-      </li>`
+    initRestaurantElementsArray: function initRestaurantElementsArray(restaurants) {
+      return restaurants.map((restaurant) => this.getRestaurantLiElement(restaurant));
     },
-    initRestaurantHTMLArray: function(restaurants) {
-      return restaurants.map(restaurant => this.getRestaurantHTML(restaurant))
-    },*/
-    initRestaurantElementsArray: function(restaurants) {
-      return restaurants.map(restaurant => this.getRestaurantLiElement(restaurant))
-    },
-    /*addRestaurantsToDOM: function(restaurantsHTMLArray) {
-      this.list.innerHTML = restaurantsHTMLArray.join('');
-    },*/
-    getRestaurantListElement: function(restaurantElementsArray) {
+    getRestaurantListElement: (restaurantElementsArray) => {
       // create new list element
       const list = document.createElement('ul');
-      this.list = list;
       list.id = 'restaurants-list';
-
       // add all <li> restaurant elements to new list
       restaurantElementsArray.forEach((element) => {
         list.appendChild(element);
       });
-
       // return new <ul> element with all restaurant <li>'s
       return list;
-      //this.listContainer.appendChild(list);
     },
-    addRestaurantListToDom: function(ulElement) {
+    addRestaurantListToDom: function addRestaurantListToDom(ulElement) {
       this.listContainer.appendChild(ulElement);
     },
-    changeDOMRestaurantList: function(newULelement) {
-      this.listContainer.replaceChild(newULelement, this.currentRestaurantUL);
-      this.currentRestaurantUL = newULelement;
+    updateRestaurantList: function updateRestaurantList(idArray) {
+      const filteredElements = idArray.map((id) => this.restaurantElementsArray[id]);
+      const newUlElement = this.getRestaurantListElement(filteredElements);
+      this.listContainer.replaceChild(newUlElement, this.currentRestaurantUL);
+      this.currentRestaurantUL = newUlElement;
     },
-    updateRestaurantList: function(idArray) {
-      const filteredElements = idArray.map(id => this.restaurantElementsArray[id]);
-      const newUL = this.getRestaurantListElement(filteredElements);
-      this.changeDOMRestaurantList(newUL);
-    },
-    addFilter: function(filterOptions, label) {
+    addFilter: function addFilter(filterOptions, label) {
       this.filterValues[filterOptions.filterKey] = 'All';
       this.comboBox = makeListbox(
         {
           id: filterOptions.filterKey,
           parentId: 'filter-options',
-          label: label,
+          label,
           callback: this.handleFilterSelection,
           values: Object.keys(filterOptions.values),
-        });
+        }
+      );
     },
-    handleFilterSelection: function(filterKey, value) {
+    handleFilterSelection: function handleFilterSelection(filterKey, value) {
       this.filterValues[filterKey] = value;
-      controller.filterRestaurants(this.filterValues);
-    }
-  }
+      this.controller.filterRestaurants(this.filterValues);
+    },
+  };
+
+  const controller = {
+    init: function init() {
+      model.initData().then((data) => {
+        view.init(this, {
+          coordinates: model.getMapCenterCoords(),
+          restaurants: data,
+          filters: {
+            cuisines: this.getFilterOptions('cuisine_type'),
+            neighborhoods: this.getFilterOptions('neighborhood'),
+          },
+        });
+      });
+    },
+    getFilterOptions: (filterKey) => {
+      const values = model.getValuesFor(filterKey);
+      return {
+        filterKey,
+        values,
+      };
+    },
+    filterRestaurants: (filterValues) => {
+      const list = model.filterRestaurants(filterValues);
+      view.updateRestaurantList(list);
+    },
+  };
+
 
   controller.init();
 });
